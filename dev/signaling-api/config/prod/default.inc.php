63d0fa20ae12479b7b32e586b72eeadd45f67c3d
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2025-02-24 21:36:21 +0900

    エラー周りを無効化

--- dev/signaling-api/config/prod/default.inc.php
+++ dev/signaling-api/config/prod/default.inc.php
@@ -6,10 +6,8 @@
  */
 
 // Disable error reporting
-error_reporting(E_ALL);
-ini_set("log_errors", 1);
-ini_set("display_errors", 1);
-ini_set('error_log', '/var/log/apache2/php-error.log');
+error_reporting(0);
+ini_set("display_errors", 0);
 
 /**
  * Each environment(dev, prod) should contain two files default.inc.php and config.inc.php.
