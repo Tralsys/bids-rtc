ba2e94ba37ebbf6fc02bd4cc4b0cef4af23a6597
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2025-02-24 23:41:28 +0900

    インデントをタブ化

--- dev/signaling-api/lib/App/RegisterDependencies.php
+++ dev/signaling-api/lib/App/RegisterDependencies.php
@@ -38,102 +38,102 @@ namespace dev_t0r\bids_rtc\signaling\App;
  */
 final class RegisterDependencies
 {
-    /**
-     * Adds dependency definitions.
-     *
-     * @param \DI\ContainerBuilder $containerBuilder Container builder.
-     *
-     * @see https://php-di.org/doc/php-definitions.html
-     */
-    public function __invoke(\DI\ContainerBuilder $containerBuilder): void
-    {
-        $containerBuilder->addDefinitions([
-            // Response factory required as typed argument in next ErrorMiddleware injection
-            \Psr\Http\Message\ResponseFactoryInterface::class => \DI\factory([\Slim\Factory\AppFactory::class, 'determineResponseFactory']),
+	/**
+	 * Adds dependency definitions.
+	 *
+	 * @param \DI\ContainerBuilder $containerBuilder Container builder.
+	 *
+	 * @see https://php-di.org/doc/php-definitions.html
+	 */
+	public function __invoke(\DI\ContainerBuilder $containerBuilder): void
+	{
+		$containerBuilder->addDefinitions([
+			// Response factory required as typed argument in next ErrorMiddleware injection
+			\Psr\Http\Message\ResponseFactoryInterface::class => \DI\factory([\Slim\Factory\AppFactory::class, 'determineResponseFactory']),
 
-            // Slim error middleware
-            // @see https://www.slimframework.com/docs/v4/middleware/error-handling.html
-            \Slim\Middleware\ErrorMiddleware::class => \DI\autowire()
-                ->constructorParameter('displayErrorDetails', \DI\get('slim.displayErrorDetails'))
-                ->constructorParameter('logErrors', \DI\get('slim.logErrors'))
-                ->constructorParameter('logErrorDetails', \DI\get('slim.logErrorDetails'))
-                ->constructorParameter('logger', \DI\get(\Psr\Log\LoggerInterface::class)),
+			// Slim error middleware
+			// @see https://www.slimframework.com/docs/v4/middleware/error-handling.html
+			\Slim\Middleware\ErrorMiddleware::class => \DI\autowire()
+				->constructorParameter('displayErrorDetails', \DI\get('slim.displayErrorDetails'))
+				->constructorParameter('logErrors', \DI\get('slim.logErrors'))
+				->constructorParameter('logErrorDetails', \DI\get('slim.logErrorDetails'))
+				->constructorParameter('logger', \DI\get(\Psr\Log\LoggerInterface::class)),
 
-            // CORS
-            \Neomerx\Cors\Contracts\AnalysisStrategyInterface::class => \DI\create(\Neomerx\Cors\Strategies\Settings::class)
-                ->method('setData', \DI\get('cors.settings')),
+			// CORS
+			\Neomerx\Cors\Contracts\AnalysisStrategyInterface::class => \DI\create(\Neomerx\Cors\Strategies\Settings::class)
+				->method('setData', \DI\get('cors.settings')),
 
-            \Neomerx\Cors\Contracts\AnalyzerInterface::class => \DI\factory([\Neomerx\Cors\Analyzer::class, 'instance']),
+			\Neomerx\Cors\Contracts\AnalyzerInterface::class => \DI\factory([\Neomerx\Cors\Analyzer::class, 'instance']),
 
-            // PDO class for database managing
-            \PDO::class => \DI\create()
-                ->constructor(
-                    \DI\get('pdo.dsn'),
-                    \DI\get('pdo.username'),
-                    \DI\get('pdo.password'),
-                    \DI\get('pdo.options'),
-                ),
+			// PDO class for database managing
+			\PDO::class => \DI\create()
+				->constructor(
+					\DI\get('pdo.dsn'),
+					\DI\get('pdo.username'),
+					\DI\get('pdo.password'),
+					\DI\get('pdo.options'),
+				),
 
-            // Monolog Logger
-            \Psr\Log\LoggerInterface::class => \DI\factory(function (string $mode, string $name, string $path, $level, array $options = []) {
-                $logger = new \Monolog\Logger($name);
+			// Monolog Logger
+			\Psr\Log\LoggerInterface::class => \DI\factory(function (string $mode, string $name, string $path, $level, array $options = []) {
+				$logger = new \Monolog\Logger($name);
 
-                $handlers = [];
-                // stream logger as default handler across all environments
-                // somebody might not need it during development
-                $handlers[] = new \Monolog\Handler\RotatingFileHandler(
-                    filename: $path,
-                    level: $level,
-                    filenameFormat: '{filename}.{date}.log',
-                );
+				$handlers = [];
+				// stream logger as default handler across all environments
+				// somebody might not need it during development
+				$handlers[] = new \Monolog\Handler\RotatingFileHandler(
+					filename: $path,
+					level: $level,
+					filenameFormat: '{filename}.{date}.log',
+				);
 
-                $logger->setTimezone(new \DateTimeZone('UTC'));
+				$logger->setTimezone(new \DateTimeZone('UTC'));
 
-                $formatter = new \Monolog\Formatter\LineFormatter(
-                    "[%datetime%] %channel%.%level_name%: %message% %extra%\n",
-                );
-                $handlers[0]->setFormatter($formatter);
+				$formatter = new \Monolog\Formatter\LineFormatter(
+					"[%datetime%] %channel%.%level_name%: %message% %extra%\n",
+				);
+				$handlers[0]->setFormatter($formatter);
 
-                $logger->pushProcessor(new \Monolog\Processor\PsrLogMessageProcessor);
-                $logger->pushProcessor(new \Monolog\Processor\WebProcessor);
-                $logger->pushProcessor(new \Monolog\Processor\MemoryUsageProcessor);
-                $logger->pushProcessor(new \Monolog\Processor\IntrospectionProcessor);
+				$logger->pushProcessor(new \Monolog\Processor\PsrLogMessageProcessor);
+				$logger->pushProcessor(new \Monolog\Processor\WebProcessor);
+				$logger->pushProcessor(new \Monolog\Processor\MemoryUsageProcessor);
+				$logger->pushProcessor(new \Monolog\Processor\IntrospectionProcessor);
 
-                if ($mode === 'development') {
-                    // add dev handlers if necessary
-                    // @see https://github.com/Seldaek/monolog/blob/f2f66cd480df5f165391ff9b6332700d467b25ac/doc/02-handlers-formatters-processors.md#logging-in-development
-                } elseif ($mode === 'production') {
-                    // add prod handlers
-                    // @see https://github.com/Seldaek/monolog/blob/f2f66cd480df5f165391ff9b6332700d467b25ac/doc/02-handlers-formatters-processors.md#send-alerts-and-emails
-                    // handlers which doesn't make sense during development
-                    // Slack, Sentry, Swift or native mailer
-                }
+				if ($mode === 'development') {
+					// add dev handlers if necessary
+					// @see https://github.com/Seldaek/monolog/blob/f2f66cd480df5f165391ff9b6332700d467b25ac/doc/02-handlers-formatters-processors.md#logging-in-development
+				} elseif ($mode === 'production') {
+					// add prod handlers
+					// @see https://github.com/Seldaek/monolog/blob/f2f66cd480df5f165391ff9b6332700d467b25ac/doc/02-handlers-formatters-processors.md#send-alerts-and-emails
+					// handlers which doesn't make sense during development
+					// Slack, Sentry, Swift or native mailer
+				}
 
-                return $logger->setHandlers($handlers);
-            })
-                ->parameter('mode', \DI\get('mode'))
-                ->parameter('name', \DI\get('logger.name'))
-                ->parameter('path', \DI\get('logger.path'))
-                ->parameter('level', \DI\get('logger.level'))
-                ->parameter('options', \DI\get('logger.options')),
+				return $logger->setHandlers($handlers);
+			})
+				->parameter('mode', \DI\get('mode'))
+				->parameter('name', \DI\get('logger.name'))
+				->parameter('path', \DI\get('logger.path'))
+				->parameter('level', \DI\get('logger.level'))
+				->parameter('options', \DI\get('logger.options')),
 
-            // Firebase
-            \Kreait\Firebase\Factory::class => \DI\factory(function (string $projectId, string $serviceAccountFile, \Psr\Log\LoggerInterface $logger, ?string $apiTokenCacheDir, ?string $authPubKeyCacheDir) {
-                $factory = (new \Kreait\Firebase\Factory())
-                    ->withAuthTokenCache(new \Symfony\Component\Cache\Adapter\FilesystemAdapter(directory: $apiTokenCacheDir))
-                    ->withVerifierCache(new \Symfony\Component\Cache\Adapter\FilesystemAdapter(directory: $authPubKeyCacheDir))
-                    ->withProjectId($projectId)
-                    ->withServiceAccount($serviceAccountFile)
-                    ->withHttpLogger($logger);
-                return $factory;
-            })
-                ->parameter('projectId', \DI\get('firebase.project_id'))
-                ->parameter('serviceAccountFile', \DI\get('firebase.sa_file'))
-                ->parameter('logger', \DI\get(\Psr\Log\LoggerInterface::class))
-                ->parameter('apiTokenCacheDir', \DI\get('firebase.api_token_cache_dir'))
-                ->parameter('authPubKeyCacheDir', \DI\get('firebase.auth.pubkey_cache_dir'))
-            ,
-            \Kreait\Firebase\Contract\Auth::class => \DI\factory([\Kreait\Firebase\Factory::class, 'createAuth']),
-        ]);
-    }
+			// Firebase
+			\Kreait\Firebase\Factory::class => \DI\factory(function (string $projectId, string $serviceAccountFile, \Psr\Log\LoggerInterface $logger, ?string $apiTokenCacheDir, ?string $authPubKeyCacheDir) {
+				$factory = (new \Kreait\Firebase\Factory())
+					->withAuthTokenCache(new \Symfony\Component\Cache\Adapter\FilesystemAdapter(directory: $apiTokenCacheDir))
+					->withVerifierCache(new \Symfony\Component\Cache\Adapter\FilesystemAdapter(directory: $authPubKeyCacheDir))
+					->withProjectId($projectId)
+					->withServiceAccount($serviceAccountFile)
+					->withHttpLogger($logger);
+				return $factory;
+			})
+				->parameter('projectId', \DI\get('firebase.project_id'))
+				->parameter('serviceAccountFile', \DI\get('firebase.sa_file'))
+				->parameter('logger', \DI\get(\Psr\Log\LoggerInterface::class))
+				->parameter('apiTokenCacheDir', \DI\get('firebase.api_token_cache_dir'))
+				->parameter('authPubKeyCacheDir', \DI\get('firebase.auth.pubkey_cache_dir'))
+			,
+			\Kreait\Firebase\Contract\Auth::class => \DI\factory([\Kreait\Firebase\Factory::class, 'createAuth']),
+		]);
+	}
 }
